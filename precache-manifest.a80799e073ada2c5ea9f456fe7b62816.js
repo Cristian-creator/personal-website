6a73e41921cdf77d235537db184ffea247f50127
Cristian-creator <cristian_upt@yahoo.com>
2021-06-21 14:05:24 +0300

    Updates

---
+++ precache-manifest.a80799e073ada2c5ea9f456fe7b62816.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90b61e883a3b726f7e4883969872e32",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "2dc51c160a16c6454db9",
+    "url": "/personal-website/static/css/2.af04be4e.chunk.css"
+  },
+  {
+    "revision": "474939ffd2ad7948ead1",
+    "url": "/personal-website/static/css/main.997c2361.chunk.css"
+  },
+  {
+    "revision": "2dc51c160a16c6454db9",
+    "url": "/personal-website/static/js/2.fe54f74e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personal-website/static/js/2.fe54f74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474939ffd2ad7948ead1",
+    "url": "/personal-website/static/js/main.e8863046.chunk.js"
+  },
+  {
+    "revision": "138eb5f6e66c1fe39a68",
+    "url": "/personal-website/static/js/runtime-main.b6919a31.js"
+  },
+  {
+    "revision": "69d90496bfcc733746664e227f60f102",
+    "url": "/personal-website/static/media/3F3D.69d90496.gif"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/personal-website/static/media/beach.aebaec51.jpeg"
+  },
+  {
+    "revision": "c487b4c7f99bad38449039ebeaeefc44",
+    "url": "/personal-website/static/media/beach.c487b4c7.gif"
+  },
+  {
+    "revision": "2315c0cfb33ef4bce2ea3e5d95337d8a",
+    "url": "/personal-website/static/media/demo-beach.2315c0cf.png"
+  },
+  {
+    "revision": "39371c7df4ef102f5328bb574dcce32b",
+    "url": "/personal-website/static/media/demo-expense-tracker.39371c7d.png"
+  },
+  {
+    "revision": "f94fd1d25cc15505d320c3c5dfe63ebb",
+    "url": "/personal-website/static/media/demo-first.f94fd1d2.png"
+  },
+  {
+    "revision": "031d9cfe09c070129929cf1eb4754871",
+    "url": "/personal-website/static/media/demo-lyrics.031d9cfe.png"
+  },
+  {
+    "revision": "e58c0770467a811fcfefd46ceaca122d",
+    "url": "/personal-website/static/media/demo-networking.e58c0770.png"
+  },
+  {
+    "revision": "6b822c742794ca50d8106acb8df310be",
+    "url": "/personal-website/static/media/demo-news-2.6b822c74.png"
+  },
+  {
+    "revision": "16c424e781c5a6fb583ccb3906772052",
+    "url": "/personal-website/static/media/demo-second.16c424e7.png"
+  },
+  {
+    "revision": "2664120207caca5972fcd8aab5338472",
+    "url": "/personal-website/static/media/demo-travel.26641202.gif"
+  },
+  {
+    "revision": "61ab150dbeda09972d8b43f177af43a4",
+    "url": "/personal-website/static/media/expenses.61ab150d.gif"
+  },
+  {
+    "revision": "cdb5b36e0e0b1916b20da72ffa834613",
+    "url": "/personal-website/static/media/expenses.cdb5b36e.jpg"
+  },
+  {
+    "revision": "b8cc507b65926e263b2782cd47400b9b",
+    "url": "/personal-website/static/media/footer.b8cc507b.jpeg"
+  },
+  {
+    "revision": "ba22dee9c26994bef7dfdf3d8e5963ee",
+    "url": "/personal-website/static/media/header.ba22dee9.webp"
+  },
+  {
+    "revision": "0ae11d54084afe1213f1a9bc18023212",
+    "url": "/personal-website/static/media/knowledge.0ae11d54.jpg"
+  },
+  {
+    "revision": "e8fba86b920a2f8488a5ad27c0c45d96",
+    "url": "/personal-website/static/media/lyrics-gif.e8fba86b.gif"
+  },
+  {
+    "revision": "86cfed20cfa64d541be950ba0d4b7d0b",
+    "url": "/personal-website/static/media/lyrics.86cfed20.jpeg"
+  },
+  {
+    "revision": "acf4b30031b7027de1b11314565c1d94",
+    "url": "/personal-website/static/media/networking.acf4b300.jpg"
+  },
+  {
+    "revision": "1791d76a9200cd42e039ac2d9871396f",
+    "url": "/personal-website/static/media/news.1791d76a.gif"
+  },
+  {
+    "revision": "9a32bf1f18c02bdd5a36813d8c05376d",
+    "url": "/personal-website/static/media/news.9a32bf1f.jpeg"
+  },
+  {
+    "revision": "55e83371dbf50a5f027b1fe31a054ad3",
+    "url": "/personal-website/static/media/nodejs.55e83371.png"
+  },
+  {
+    "revision": "4603f0d1d6482913bccc9693c93bcf5a",
+    "url": "/personal-website/static/media/react.4603f0d1.png"
+  },
+  {
+    "revision": "e2399fafe01546a5724d5c8eec0bc79e",
+    "url": "/personal-website/static/media/school-responsive.e2399faf.jpeg"
+  },
+  {
+    "revision": "48bfd9992d2ff364756e752231dffd19",
+    "url": "/personal-website/static/media/school-responsive2.48bfd999.jpeg"
+  },
+  {
+    "revision": "28829a5dd101b119ef549e1a86bdd307",
+    "url": "/personal-website/static/media/travel.28829a5d.gif"
+  },
+  {
+    "revision": "5e24e5e57dd9b1aedccedc33a12597c3",
+    "url": "/personal-website/static/media/travel.5e24e5e5.jpg"
+  }
+]);
